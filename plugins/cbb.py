1fb2b32b2839c423fb5c86f59488fa2f92d38861
Miner-official <134123045+Miner-official@users.noreply.github.com>
2023-07-26 10:15:26 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ ᴍʏ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>♡ 𝚂𝙰𝙷𝙸𝙻 ♡</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://t.me/itz_sahil_official'>ᴘʀɪᴠᴀᴛᴇ ᴄᴏᴅᴇ 🥲</a>\n○ ᴍʏ ᴄʜᴀɴɴᴇʟ : @sahil_official_here ❤️‍🔥\n○ ᴅᴏɴᴀᴛᴇ ᴍᴇ : https://t.me/sahil_official_here/79</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
